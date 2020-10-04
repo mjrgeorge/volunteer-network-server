4c84c6b1126e2e3251b3949e54970581a1954539
mjrgeorge <mjrgeorge@gmail.com>
2020-10-04 10:54:53 +0600

    ui to database post success

--- index.js
+++ index.js
@@ -6,26 +6,34 @@ const uri = "mongodb+srv://volunteer-network:volunteerNetwork2020@cluster0.bqmcj
 
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(cors())
 
 const port = 5000
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 client.connect(err => {
   const usersCollection = client.db("volunteerNetwork").collection("users");
-  const data = {name: 'Jubayer', email: 'jubayer@gmail.com'};
-  usersCollection.insertOne(data)
-  .then(result => {
-    console.log(result);
-  })
-});
 
+  app.post('/addUser', (req, res) => {
+    const document = req.body;
+    usersCollection.insertOne(document)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+        console.log(result);
+      })
+  })
+
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
